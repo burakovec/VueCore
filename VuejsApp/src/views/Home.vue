5c010c66c7401aea24cb9100fa25acff8d3711bc
Burak ÖVEÇ <burakovecc@outlook.com>
2021-10-04 23:52:13 +0300

    code

--- VuejsApp/src/views/Home.vue
+++ VuejsApp/src/views/Home.vue
@@ -14,18 +14,54 @@
       <b-card-text>We carefully crafted JWT flow so you can implement JWT with ease and with minimum efforts.</b-card-text>
       <b-card-text>Please read our  JWT Documentation to get more out of JWT authentication.</b-card-text>
     </b-card>
+    <b-button
+      type="submit"
+      variant="primary"
+      block
+      @click="getWeforecast"
+    >
+      İstek Yolla
+    </b-button>
+    <b-card title="🔒">
+      <b-card-text  v-for="(item,i) in content" :key="i">{{ item }}</b-card-text>
+    </b-card>
   </div>
 </template>
 
 <script>
-import { BCard, BCardText, BLink } from 'bootstrap-vue'
+import {
+  BCard, BCardText, BLink, BButton,
+} from 'bootstrap-vue'
 
 export default {
   components: {
     BCard,
     BCardText,
     BLink,
+    BButton,
   },
+  data() {
+    return {
+      content: [],
+    }
+  },
+  methods: {
+    async getWeforecast() {
+      var axios = require('axios');
+
+      var config = {
+        method: 'get',
+        url: 'http://localhost:8099/WeatherForecast',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      };
+
+     var response = await axios(config)
+      this.content = response.data
+    },
+  },
+
 }
 </script>
 
